9389a33c4a3ae2f42bfa0194f37f8c210c8b20b6
Tom Saporito <tamb89@protonmail.com>
2021-10-17 00:35:12 -0400

    adds fullscreen lowers vibrationtime

--- src/index.js
+++ src/index.js
@@ -5,9 +5,46 @@ const GameController = {
     );
     buttons.forEach((button) => {
       button.addEventListener("click", () => {
-        navigator.vibrate(100);
+        navigator.vibrate(10);
       });
     });
+
+    var fullscreen = false;
+
+    var elem = document.documentElement;
+
+    document
+      .getElementById("fullscreen")
+      .addEventListener("click", function () {
+        fullscreen = !fullscreen;
+        fullscreen ? openFullscreen() : closeFullscreen();
+      });
+
+    /* View in fullscreen */
+    function openFullscreen() {
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.webkitRequestFullscreen) {
+        /* Safari */
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) {
+        /* IE11 */
+        elem.msRequestFullscreen();
+      }
+    }
+
+    /* Close fullscreen */
+    function closeFullscreen() {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) {
+        /* Safari */
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+        /* IE11 */
+        document.msExitFullscreen();
+      }
+    }
   },
 };
 
