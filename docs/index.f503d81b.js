1adbc3415e4da42ab91cb4e539a0e170193322df
Tom Saporito <tamb89@protonmail.com>
2021-10-17 00:24:44 -0400

    updates parcel build

---
+++ docs/index.f503d81b.js
@@ -0,0 +1,2 @@
+var GameController={init:function(){document.querySelectorAll(".game-controller__wrapper button").forEach((function(n){n.addEventListener("click",(function(){navigator.vibrate(100)}))}))}};document.addEventListener("DOMContentLoaded",(function(){GameController.init()}));
+//# sourceMappingURL=index.f503d81b.js.map
