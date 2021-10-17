9a9cb0db95df3c0e61975368128cb1d75b9cbcef
Tom Saporito <tamb89@protonmail.com>
2021-10-17 16:20:08 -0400

    removes react

--- src/react/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import GameController from "./components/GameController";
-
-const App = document.getElementById("app");
-
-ReactDOM.render(<GameController />, App);
